81341d8c868f4f547543fab83f27643efb1f8e9a
Aleksandra <aleksandra.hinc00@gmail.com>
2022-10-25 20:16:52 +0200

    ForEach

---
+++ src/ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String [] catTable=new String[] {"Bolek", "Lolek", "Ziomek", "Leszek"};
+        for (String cat: catTable) {
+            System.out.println(cat);
+        }
+    }
+}
