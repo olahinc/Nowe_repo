f6f8e4965bd0456699ad2acc5e99e9a232970321
Aleksandra <aleksandra.hinc00@gmail.com>
2022-10-25 20:07:21 +0200

    Tablica kotkow

--- src/For_loop.java
+++ src/For_loop.java
@@ -1,7 +1,7 @@
 public class For_loop {
     public static void main(String[] args) {
         String [] tablicaKotkow=new String[] {"Bolek", "Lolek", "Ziomek", "Leszek"};
-        for (int i=0; i<4; i++) {
+        for (int i=0; i<tablicaKotkow.length; i++) {
             System.out.println(tablicaKotkow[i]);
         }
         }
